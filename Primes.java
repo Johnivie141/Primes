5da79b962e60e9e6e12f59adbfefc713eb752065
Johnivie141 <john.ivie@gmail.com>
2017-04-07 11:27:59 -0600

    Update Primes.java

--- Primes.java
+++ Primes.java
@@ -5,13 +5,15 @@ class Primes
 	public static void main(String args[]){
 
 		int columns = Integer.parseInt(args[0]) +1;
+		// maybe we will change this later for 3x100 etc.
+		int rows=columns;
 		if (columns<1)
 		{
 		  System.out.println("please enter a number greater than 1");
 		  return;
 		}
 		// number of Primes we need to find
-		int numberOfPrimes= columns *columns;
+		int numberOfPrimes= columns *rows;
 
 		
 		// upper bound on how many numbers to check to find our primes
@@ -29,9 +31,11 @@ class Primes
 		// when done 
 		for (int i=2;i*i<=max;i++)
 		{
+			//skip all numbers already marked
 			for (;Numbers[i]==1;i++);
 			
 		   
+			
 			lastPrime=i;
 			primes.add(lastPrime);
 			// if we have reached the square root of max we are finished
@@ -42,18 +46,21 @@ class Primes
 			}
 		}
                 // It may be better to wrap this into the output and forget the ArrayList<Integer> primes ??
+		// complete the list of primes
 		for (int j=lastPrime;j<max;j++)	{
 			 
 			if (Numbers[j]==0){
+				// perhaps stop when we have enough?
 				primes.add(j);
 				lastPrime=j;
 			}
 			
 		}
+		// find the biggest number we might display and use that for formatting
 		int maxSize=("" + lastPrime).length();
 			int primesSize=primes.size();
 			
-		for (int row=0;row <columns;row++)
+		for (int row=0;row <rows;row++)
 		{
 			StringBuilder sb = new StringBuilder("");
 			for (int primeIndex=row *columns;primeIndex< row *columns +columns && primeIndex <primesSize;primeIndex++)
